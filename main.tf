435156e63731dd8977f18942e38cbe74d5f01c11
Daniel-MundosE <mundose@ojcius.com>
2021-07-20 21:39:50 -0300

    cambios en main de terraform por zona no disponible

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   version = "~> 2.7"
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "web" {  
-  ami = "ami-077e31c4939f6a2f3"
+  ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
 }
 
