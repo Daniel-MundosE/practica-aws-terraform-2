31138c3e2064824d7e477ebb3ddef7db16732049
Daniel-MundosE <mundose@ojcius.com>
2021-07-20 21:46:32 -0300

    tags añadidos a la instancia. duplicado de EC2

--- main.tf
+++ main.tf
@@ -4,8 +4,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {  
+  count = 2
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "MundosE-2103-${count.index}"
+  }
+
 }
 
 output "public_ip" {
